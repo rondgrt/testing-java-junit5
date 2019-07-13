031aa1fa9eb0fe6a8c26729fba83846001ce0e19
Ron de Groot <rondgrt@gmail.com>
2019-07-13 14:21:59 +0200

    FOLLOW ALONG adding tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    private IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", controller.index());
+        assertEquals("index", controller.index(), "Wrong view name returned");
+    }
+
+    @Test
+    void oupsHandler() {
+    }
+}
---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+}
