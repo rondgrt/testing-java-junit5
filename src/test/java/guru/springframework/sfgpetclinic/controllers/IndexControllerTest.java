365f83d04ccda6a7012d5c0e0214b14a5141b4f7
Ron de Groot <rondgrt@gmail.com>
2019-07-14 11:53:03 +0200

    FOLLOW ALONG adding tests

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,12 +16,15 @@ class IndexControllerTest {
     }
 
     @Test
-    void index() {
-        assertEquals("index", controller.index());
-        assertEquals("index", controller.index(), "Wrong view name returned");
+    @DisplayName("Test exception")
+    void oupsHandler() {
+        assertEquals("notimplemented", controller.oupsHandler());
     }
 
     @Test
-    void oupsHandler() {
+    @DisplayName("Test proper View name is returned for index page")
+    void index() {
+        assertEquals("index", controller.index());
+        assertEquals("index", controller.index(), "Wrong view name returned");
     }
 }
--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,7 +1,28 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OwnerTest {
+
+    @Disabled(value = "ff uitgezet omdat het kan")
+    @Test
+    void dependentAssertions() {
+
+        Owner owner = new Owner(1l, "Joe", "Buck");
+        owner.setCity("Key West");
+        owner.setTelephone("123456789");
+
+        assertAll("Person Properties",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Joee", owner.getFirstName(), "First name did not match"),
+                        () -> assertEquals("Buck", owner.getLastName())),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Key Westt", owner.getCity(), "City did not match"),
+                        () -> assertEquals("123456789", owner.getTelephone())
+                ));
+    }
 
 }
